a2c349ba7dbbf82a2f93b9cd1da23746e9380f48
IvanShuklin <157677279+IvanShuklin@users.noreply.github.com>
2025-10-10 22:15:59 +0300

    Add mock data

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const EVENT_TYPES = [
+  'taxi',
+  'bus',
+  'train',
+  'ship',
+  'drive',
+  'flight',
+  'check-in',
+  'sightseeing',
+  'restaurant',
+];
---
+++ src/mock/points.js
@@ -0,0 +1,55 @@
+import { getRandomArrayElement } from '../utils.js';
+import { EVENT_TYPES } from '../const.js';
+
+export const mockPoints = [
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808c',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T22:55:56.845Z',
+    date: '2019-07-11T11:22:13.375Z',
+    destination: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+    favorite: false,
+    offers: ['b4c3e4e6-9053-42ce-b747-e281314baa31'],
+    type: getRandomArrayElement(EVENT_TYPES),
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808d',
+    basePrice: 2800,
+    dateFrom: '2019-07-10T22:55:56.845Z',
+    date: '2019-07-11T11:22:13.375Z',
+    destination: 'cfe416cq-10xa-ye10-8077-2fs9a01edcab',
+    favorite: true,
+    offers: ['b4c3e4e6-9053-42ce-b747-e281314baa31'],
+    type: getRandomArrayElement(EVENT_TYPES),
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808e',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T22:55:56.845Z',
+    date: '2019-07-11T11:22:13.375Z',
+    destination: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+    favorite: false,
+    offers: ['b4c3e4e6-9053-42ce-b747-e281314baa31'],
+    type: getRandomArrayElement(EVENT_TYPES),
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808e',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T22:55:56.845Z',
+    date: '2019-07-11T11:22:13.375Z',
+    destination: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+    favorite: false,
+    offers: ['b4c3e4e6-9053-42ce-b747-e281314baa31'],
+    type: getRandomArrayElement(EVENT_TYPES),
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808e',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T22:55:56.845Z',
+    date: '2019-07-11T11:22:13.375Z',
+    destination: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+    favorite: false,
+    offers: ['b4c3e4e6-9053-42ce-b747-e281314baa31'],
+    type: getRandomArrayElement(EVENT_TYPES),
+  },
+];
